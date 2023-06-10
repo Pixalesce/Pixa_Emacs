598ac5b7be9c594e699662c8f5e124624a63de2b
Pixalesce <pixalesce@gmail.com>
2023-06-10 17:49:20 +0800

    initial commit

---
+++ init.el
@@ -0,0 +1,8 @@
+;; Copyright (C) 2023 Pixalesce
+;;
+;; Author: Pixalesce <pixalesce@gmail.com>
+;; Maintainer: Pixalesce <pixalesce@gmail.com>
+;; Created: June 10, 2023
+;; Modified: June 10, 2023
+;; Homepage: https://github.com/Pixalesce/Emacs_Config
+;; Package-Requires: ((emacs "24.3"))
